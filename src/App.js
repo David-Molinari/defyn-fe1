1e31ec9fcee1e477d6d625195b17af1471de9661
David Molinari <david.m.molinari@gmail.com>
2021-03-03 16:45:45 -0500

    Updates to functionality and styling

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import "./App.css";
 import AsyncSelect from "react-select/async";
 import ReactPlayer from "react-player";
 import Axios from "axios";
-import FacebookIcon from "./images/FacebookIcon.png";
-import InstagramIcon from "./images/InstagramIcon.png";
-import TwitterIcon from "./images/TwitterIcon.png";
 
-function App() {
+export default function App() {
+
   const [company, setCompany] = useState({
     id: -1,
     URL: "",
-    Name: "",
-    Socials: "",
     Contact: ""
   })
+
   const [videos, setVideos] = useState([])
+  
   const [videoCurrent, setVideoCurrent] = useState({
     Link: "",
     Name: ""
   })
 
-  let url = window.location.href.slice((window.location.href.search("/") + 2), -1)
+  let url = window.location.href.slice(
+    (window.location.href.search("/") + 2), -1
+  )
 
   if (url == 'localhost:3000') {
     url = 'energyti.me'
@@ -47,10 +47,12 @@ function App() {
       .get(`http://localhost:5000/api/videos/${comp.id}`)
       .then(res => {
           setVideos(res.data)
-          setVideoCurrent({Link: res.data[0].Link, Name: res.data[0].Name})
+          setVideoCurrent({
+            Link: res.data[0].Link, 
+            Name: res.data[0].Name
+          })
       })
       .catch(err => {
-
       })
   }
 
@@ -64,56 +66,33 @@ function App() {
   }
 
   async function handleChange(data) {
-      let videoLink = await videos.find(video => video.Name == data.value).Link
+      let videoLink = await videos.find(
+        video => video.Name == data.value
+      ).Link
       try {
-        setVideoCurrent({Link: videoLink, Name: data.value})
+        setVideoCurrent({
+          Link: videoLink, 
+          Name: data.value
+        })
       }
       catch {
-        console.log("error")
       }
   }
 
-  console.log(window.location.href.slice((window.location.href.search("/") + 2), -1))
-  console.log(company.Socials.slice(company.Socials.search("Twitter.com")))
-  console.log(company.Socials.slice(company.Socials.search("Facebook.com"), company.Socials.search(",")))
-
   return (
-    <div>
-      <div id="TopSection">
+    <div id="AppContainer">
+      <div id="SelectInputContainer">
         <AsyncSelect
-          name="Video select input"
           className="SelectInput"
           classNamePrefix="SelectInput"
           defaultOptions
-          cacheOptions
           value={videoCurrent.Name}
           inputValue={videoCurrent.Name}
           loadOptions={getOptions}
           onChange={(data) => handleChange(data)}
           placeholder=""
         />
-        <div id="CompanySocials">
-          <a 
-              href={`https://facebook.com/${company.Socials.slice(0, company.Socials.search("1"))}`}
-              target="_blank"
-            >
-            <img id="FacebookIcon" src={FacebookIcon} alt="Facebook Icon"/>
-          </a>
-          <a 
-            href={`https://instagram.com/${company.Socials.slice((company.Socials.search("1")+1), (company.Socials.search("2")))}`}
-            target="_blank"
-          >
-            <img id="InstagramIcon" src={InstagramIcon} alt="Instagram Icon"/>
-          </a>
-          <a 
-            href={`https://twitter.com/${company.Socials.slice((company.Socials.search("2")+1))}`}
-            target="_blank"
-          >
-            <img id="TwitterIcon" src={TwitterIcon} alt="Twitter Icon"/>
-          </a>
-        </div>
-     </div>
-     <div id="MiddleSection">
+      </div>
       <div id="VideoContainer">
           <ReactPlayer
               key={videoCurrent.Name}
@@ -121,14 +100,10 @@ function App() {
               controls={true}
               playsinline={true}
           />
-        </div>
-     </div>
-     <div id="BottomSection">
-        <h1 id="CompanyName">{company.Name}</h1>
+      </div>
+      <div id="ContactContainer">
         <h3 id="CompanyEmail">{company.Contact}</h3>
-     </div>
+      </div>
     </div>
   );
 }
-
-export default App;
